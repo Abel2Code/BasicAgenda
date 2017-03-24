5b10069cb1c2dd6cd54119f123b93e5cafc14ca6
Abel2Code <asalinaswc1@aol.com>
2017-03-24 11:30:18 -0700

    Removed date validation method

--- Week.java
+++ Week.java
@@ -13,42 +13,7 @@ public class Week {
 	private List<String> friday = new ArrayList<String>();
 	private List<String> saturday = new ArrayList<String>();
 	
-	public Week(String date){
-		this.date = date;
-	}
-	
-	public static boolean validateDate(String date1, String date2){
-		int tempMonth1 = Integer.parseInt(date1.substring(0, date1.indexOf('/')));
-		int tempMonth2 = Integer.parseInt(date2.substring(0, date2.indexOf('/')));;
-		int tempDay1 = Integer.parseInt(date1.substring(0, date1.indexOf('/')).substring(0, date1.indexOf('/')));
-		int tempDay2 = Integer.parseInt(date2.substring(0, date2.indexOf('/')).substring(0, date2.indexOf('/')));;
-		int tempYear1 = Integer.parseInt(date1.substring(0, date1.indexOf('/')).substring(0, date1.indexOf('/')).substring(date1.indexOf('/')));
-		int tempYear2 = Integer.parseInt(date2.substring(0, date2.indexOf('/')).substring(0, date2.indexOf('/')).substring(date2.indexOf('/')));
-		
-		
-		/*if(tempMonth1 > tempMonth2 || tempYear1 > tempYear2){
-			//Get here if Month or Year (1) is greater than Month or Year (2)
-			return false;
-		}else if(tempMonth1 == && tempDay1 == 31){
-			//Get here if 30 day month with day 31
-		}else if(tempMonth1 == tempMonth2){
-			//Get here if same month 
-			if(tempYear1 == tempYear2){
-				// Get here if same month and year
-				if(tempDay2 - tempDay1 == 6){
-					return true;
-				} else{
-					return false;
-				}
-			} else{
-				//Get here if same month but different year
-				return false;
-			} 
-		} else if(tempMonth1 == tempMonth2 - 1 || (tempMonth1 == 12 && tempMonth2 == 1)){
-			//Get here if valid Month to Month separation
-			if(tempMonth 
-		} else{
-			return false;
-		}*/ // 
+	public Week(String weekTitle){
+		this.weekTitle = weekTitle;
 	}
 }
