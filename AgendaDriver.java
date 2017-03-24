9567119b6c28bec67fcd160069c5d3aaeba70dc0
Abel2Code <asalinaswc1@aol.com>
2017-03-24 11:39:10 -0700

    Added basic framework for Agenda Driver

---
+++ AgendaDriver.java
@@ -0,0 +1,17 @@
+package agenda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgendaDriver {
+	private static List<Week> weeks = new ArrayList<Week>(); 
+
+	public static void main(String[] args) {	
+		startGUI();
+	}
+	
+	public static void startGUI(){
+		
+	}
+
+}
