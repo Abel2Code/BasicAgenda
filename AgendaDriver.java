88eaa58978e041825ee998e93af970e2a38a442d
Abel2Code <asalinaswc1@aol.com>
2017-03-25 22:02:50 -0700

    Added getters and setter for Week and start GUI framework for AgendaDriver

--- AgendaDriver.java
+++ AgendaDriver.java
@@ -3,15 +3,35 @@ package agenda;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class AgendaDriver {
 	private static List<Week> weeks = new ArrayList<Week>(); 
-
+	
 	public static void main(String[] args) {	
 		startGUI();
 	}
 	
 	public static void startGUI(){
-		
+		int selection;
+		String[] mainChoices = {"Quit","Create an Agenda", "View Past Weeks", "Copy a Past Week's Agenda"};
+
+		do {
+
+			selection = JOptionPane.showOptionDialog(null, "Would you like to deal with: ", "Main Menu", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE, null, mainChoices, null);
+
+			switch (selection) {
+			case 1:
+				
+				break;
+			case 2:
+				;
+				break;
+			case 3:
+				
+			}
+
+		} while (selection != 0 && selection != JOptionPane.CLOSED_OPTION);
 	}
 
 }
--- Week.java
+++ Week.java
@@ -16,4 +16,60 @@ public class Week {
 	public Week(String weekTitle){
 		this.weekTitle = weekTitle;
 	}
+
+	public List<String> getSunday() {
+		return sunday;
+	}
+
+	public void setSunday(List<String> sunday) {
+		this.sunday = sunday;
+	}
+
+	public List<String> getMonday() {
+		return monday;
+	}
+
+	public void setMonday(List<String> monday) {
+		this.monday = monday;
+	}
+
+	public List<String> getTuesday() {
+		return tuesday;
+	}
+
+	public void setTuesday(List<String> tuesday) {
+		this.tuesday = tuesday;
+	}
+
+	public List<String> getWednesday() {
+		return wednesday;
+	}
+
+	public void setWednesday(List<String> wednesday) {
+		this.wednesday = wednesday;
+	}
+
+	public List<String> getThursday() {
+		return thursday;
+	}
+
+	public void setThursday(List<String> thursday) {
+		this.thursday = thursday;
+	}
+
+	public List<String> getFriday() {
+		return friday;
+	}
+
+	public void setFriday(List<String> friday) {
+		this.friday = friday;
+	}
+
+	public List<String> getSaturday() {
+		return saturday;
+	}
+
+	public void setSaturday(List<String> saturday) {
+		this.saturday = saturday;
+	}
 }
