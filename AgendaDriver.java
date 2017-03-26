d6ad4dddbf691d45d4aeb212df7e1336749db5fd
Abel2Code <asalinaswc1@aol.com>
2017-03-26 16:27:10 -0700

    Started main GUI and added way to add to to-do list

--- AgendaDriver.java
+++ AgendaDriver.java
@@ -14,7 +14,8 @@ public class AgendaDriver {
 	
 	public static void startGUI(){
 		int selection;
-		String[] mainChoices = {"Quit","Create an Agenda", "View Past Weeks", "Copy a Past Week's Agenda"};
+		String[] mainChoices = {"Quit","Create an New Week", "View Past Weeks", "Copy a Past Week's Agenda"};
+		String[] daysOfWeek = {"Quit","Sunday","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"};
 
 		do {
 
@@ -22,7 +23,9 @@ public class AgendaDriver {
 
 			switch (selection) {
 			case 1:
-				
+				weeks.add(new Week(JOptionPane.showInputDialog("What would you like to name this Week?")));
+				selection = JOptionPane.showOptionDialog(null, "Would you like to deal with: ", "Main Menu", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE, null, mainChoices, null);
+				addToWeek(selection);
 				break;
 			case 2:
 				;
@@ -34,4 +37,29 @@ public class AgendaDriver {
 		} while (selection != 0 && selection != JOptionPane.CLOSED_OPTION);
 	}
 
+	public static void addToWeek(int selection){
+		int latestWeek = weeks.size();
+		switch (selection) {
+		case 1:
+			weeks.get(latestWeek).getSunday().add(JOptionPane.showInputDialog("What would you like to add to Sunday?"));
+			break;
+		case 2:
+			weeks.get(latestWeek).getMonday().add(JOptionPane.showInputDialog("What would you like to add to Monday?"));
+			break;
+		case 3:
+			weeks.get(latestWeek).getTuesday().add(JOptionPane.showInputDialog("What would you like to add to Tuesday?"));
+			break;
+		case 4: 
+			weeks.get(latestWeek).getWednesday().add(JOptionPane.showInputDialog("What would you like to add to Wednesday?"));
+			break;
+		case 5:
+			weeks.get(latestWeek).getThursday().add(JOptionPane.showInputDialog("What would you like to add to Thursday?"));
+			break;
+		case 6:
+			weeks.get(latestWeek).getFriday().add(JOptionPane.showInputDialog("What would you like to add to Friday?"));
+			break;
+		case 7:
+			weeks.get(latestWeek).getSaturday().add(JOptionPane.showInputDialog("What would you like to add to Saturday?"));
+		}
+	}
 }
